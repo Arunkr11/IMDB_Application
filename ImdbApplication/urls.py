b7cc780121edc25500de331fb70c76ab57971ce2
Arun <arun.k.r260@gmail.comgit config --listgit config --global user.email arun.k.r260@gmail.comgit config --global user.email arun.k.r260@gmail.com>
2024-01-26 14:00:36 +0530

    second commit

--- ImdbApplication/urls.py
+++ ImdbApplication/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/all/',views.MovieListView.as_view(),name='movie-list'),
     path('movies/<int:pk>/',views.MovieDetailView.as_view(),name='movie-detail'),
-    path('movies/<int:pk>/remove',views.MovieDeleteView.as_view(),name='movie-delete'),
+    path('movies/<int:pk>/remove/',views.MovieDeleteView.as_view(),name='movie-delete'),
     path('movies/add/',views.MoviesCreateView.as_view(),name='movie-create'),
+    path('movies/<int:pk>/change/',views.MovieUpdateView.as_view(),name='movie-update'),
 ]
+
--- myapp/views.py
+++ myapp/views.py
@@ -39,9 +39,47 @@ class MoviesCreateView(View):
         return render(request,'movie_add.html',{'form':form})
     
     def post(self, request, *args, **kwargs):
-        data={k:v for k,v in request.POST.items()}
-        data.pop('csrfmiddlewaretoken')
-        Movie.objects.create(**data)
-        return redirect('movie-list') 
-           
+        # data={k:v for k,v in request.POST.items()}
+        # data.pop('csrfmiddlewaretoken')
+        # Movie.objects.create(**data)
+        # return redirect('movie-list') 
+        form=MovieForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            Movie.objects.create(**data)
+            return redirect('movie-list')
+        else:
+            return render(request,'movie_add.html',{'form':form})   
+        
+class MovieUpdateView(View):
+    def get(self, request,*args,**kwargs):
+        id=kwargs.get('pk')
+        movie_object=Movie.objects.get(id=id)
+        data={
+            "name":movie_object.name,
+            "language":movie_object.language,
+            "run_time":movie_object.run_time,
+            "genre":movie_object.genre,
+            "director":movie_object.director,
+            "actors":movie_object.actors,
+            "year":movie_object.year
+        }
+        form=MovieForm(initial=data)
+        return render(request,'movie_edit.html',{'form':form})
     
+    
+    def post(self, request,*args,**kwargs):
+        # data={k:v for k,v in request.POST.items()}
+        # data.pop('csrfmiddlewaretoken')
+        # id=kwargs.get('pk')
+        # Movie.objects.filter(id=id).update(**data)
+        # return redirect('movie-list')
+        form=MovieForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            id=kwargs.get('pk')
+            Movie.objects.filter(id=id).update(**data)
+            return redirect('movie-list')
+        else:
+            return render(request, 'movie_list.html',{'form':form})
+            
